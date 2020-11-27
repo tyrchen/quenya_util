94aa00aa72cf2069d875ce13d5ac1c61ce822c25
Tyr Chen <tyr.chen@gmail.com>
2020-11-27 00:11:59 -0800

    add get_accept

--- lib/request_helper.ex
+++ lib/request_helper.ex
@@ -13,8 +13,16 @@ defmodule QuenyaUtil.RequestHelper do
   def get_param(conn, name, "path"), do: conn.path_params[name]
   def get_param(conn, name, "query"), do: conn.query_params[name]
 
-  def get_param(conn, name, "header") do
-    Enum.reduce_while(conn.req_headers, nil, fn {k, v}, _acc ->
+  def get_param(conn, name, position) when position in ["header", "resp_header"] do
+    headers =
+      case position do
+        "header" -> conn.req_headers
+        "resp_header" -> conn.resp_headers
+      end
+
+    name = String.downcase(name)
+
+    Enum.reduce_while(headers, nil, fn {k, v}, _acc ->
       case k == name do
         true -> {:halt, v}
         _ -> {:cont, nil}
@@ -28,5 +36,15 @@ defmodule QuenyaUtil.RequestHelper do
     v = get_param(conn, "content-type", "header")
     [result | _] = String.split(v, ";")
     result
+  end
+
+  def get_accept(conn) do
+    conn
+    |> get_param("accept", "header")
+    |> String.split(",")
+    |> Enum.map(fn part ->
+      [result | _] = part |> String.trim() |> String.split(";")
+      result
+    end)
   end
 end
